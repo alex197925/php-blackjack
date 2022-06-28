9232703dae53f911efda8c28dff65b2bbc779c41
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-06-28 09:27:26 +0200

    Add Player functionality... this part was very hard i didnt understand all

--- Classes/Player.php
+++ Classes/Player.php
@@ -3,23 +3,47 @@
 declare(strict_types=1);
 
 class Player {
-    private array $cards = [];
+    private array $cards;
     private bool $lost = false;
+
+    public  function __construct(Deck $deck) {
+
+     for ($i = 0; $i < 2; $i++) {
+         $this->cards[]=$deck->drawCard();
+     }
+
+        $this->cards[] = $deck->drawCard();
+        $this->cards[] = $deck->drawCard();
+    }
+
+
+
 
     public function hit() {
 
+        if ($this->getScore()> 21) {
+            $this->lost= true;
+        }
     }
 
     public function surrender() {
+        $this->lost= true;
 
     }
 
     public function getScore() {
+        $score = 0;
+        $cards = $this->cards;
+        foreach ($cards as $card) {
+            $score += $card->getValue();
+        }
 
     }
 
-    public function hasLost() {
 
+    public function hasLost(): bool
+    {
+        return $this->lost;
     }
 
 
